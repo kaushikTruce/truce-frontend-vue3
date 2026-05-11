81183df27e321d28ccd9302a2e89b8dada2e61e6
Kaushik Barman <kaushik@truce.io>
2026-05-11 16:15:43 +0000

    migrate main.js to use Vuetify and VPhoneInput

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import '@mdi/font/css/materialdesignicons.css';
+import VPhoneInput from 'v-phone-input';
+import 'v-phone-input/dist/v-phone-input.css';
 
-createApp(App).mount('#app')
+const vuetify = createVuetify();
+
+const app = createApp(App);
+
+app
+    .use(vuetify)
+    .use(VPhoneInput)
+    .mount('#app');
