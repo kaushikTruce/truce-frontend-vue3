7609b33f0278c2e9091ed76482fa5f6116b7684d
Kaushik Barman <kaushik@truce.io>
2026-05-11 16:16:10 +0000

    add admin.js file

---
+++ src/authentication/admins.js
@@ -0,0 +1,9 @@
+var admin_emails = [
+    'truceengineer@gmail.com',
+    'jpet88@gmail.com',
+    '15kumarviv@gmail.com',
+    'arnob.sengupta100@gmail.com',
+    'joey@truce.io'
+];
+
+export default admin_emails;
