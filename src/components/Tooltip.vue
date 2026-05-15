16b5ad84c1d2c948fbb2cb536f0dc35e29f1e409
Kaushik Barman <kaushik@truce.io>
2026-05-15 16:07:38 +0000

    migrate tooltip.vue comp to vuejs latest

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-tooltip location="top" max-width="25%">
+        <template #activator="{ props }">
+            <span v-bind="props">{{ headerText }}</span>
+        </template>
+        <span>{{ headerTool }}</span>
+    </v-tooltip>
+</template>
+
+<script setup>
+
+defineProps({
+    headerText: {
+        type: String,
+        default: '',
+    },
+    headerTool: {
+        type: String,
+        default: '',
+    }
+})
+
+
+</script>
+
+<style scoped></style>
