85df154e5b9ea32aa0bb6db8e07cd06879d28da5
Kaushik Barman <kaushik@truce.io>
2026-05-19 18:56:11 +0000

    add InfoToolTip.vue component

---
+++ src/components/InfoTooltip.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-menu location="top">
+        <template #activator="{props}">
+            <v-btn v-bind="props" icon size="x-small" variant="text">
+                <v-icon color="grey-lighten-1">
+                    mdi-information-variant-circle-outline
+                </v-icon>
+            </v-btn>
+        </template>
+
+        <v-card elevation="0" class="tooltip-card">
+            <v-card-subtitle class="pa-2 text-white">
+                {{ tooltipText }}
+            </v-card-subtitle>
+        </v-card>
+    </v-menu>
+
+
+</template>
+
+<script setup>
+
+defineProps({
+    tooltipText: {
+        type: String,
+        default: '',
+    }
+})
+
+</script>
+
+<style scoped>
+.tooltip-card {
+    background-color: #5d5b5be6 !important;
+}
+
+</style>
