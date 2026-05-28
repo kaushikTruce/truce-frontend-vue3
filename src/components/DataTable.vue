9caf67084c339b1fe162d919a9e268f990453e18
Kaushik Barman <kaushik@truce.io>
2026-05-28 17:35:15 +0000

    removed opacity value

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -290,7 +290,7 @@
       </v-menu>
 
       <template v-else>
-        <v-chip class="truckCostShip pa-0 text-center" color="transparent" disabled style="opacity: 1">
+        <v-chip class="truckCostShip pa-0 text-center" color="transparent" disabled>
           {{ formatDollars(item.avg_cogs) }}
         </v-chip>
       </template>
@@ -474,7 +474,7 @@
             </v-menu>
 
             <template v-else>
-              <v-chip class="truckCostShip pa-0 text-center" color="transparent" disabled style="opacity: 1">
+              <v-chip class="truckCostShip pa-0 text-center" color="transparent" disabled>
                 {{ formatDollars(shipment.cogs) }}
               </v-chip>
             </template>
